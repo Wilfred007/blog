b1386540e7217b9cdbc8ea8700b67ebbe80c62df
Wilfred007 <adzerwilfred007@gmail.com>
2024-04-24 04:39:22 -0700

    main

---
+++ app/components/utils/Provider.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react"
+import { ThemeProvider } from "next-themes"
+
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Provider = ({children}: Props) => {
+  return <ThemeProvider attribute="class">
+
+  </ThemeProvider>
+}
